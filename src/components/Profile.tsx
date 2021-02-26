d35a3307870011821f60c99bdca88a4f141baeb5
andregodasi <andre.godasi@gmail.com>
2021-02-25 21:11:14 -0300

    feat: add component Profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "../styles/components/Profile.module.css";
+const Profile: React.FC = () => {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/andregodasi.png" alt="André" />
+      <div>
+        <strong>André Gonçalves</strong>
+        <p>
+          <img src="icons/level.svg" alt="level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
