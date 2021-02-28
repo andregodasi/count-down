24b282130f1a48f84c93450299c60014a9c0ecb2
andregodasi <andre.godasi@gmail.com>
2021-02-28 12:49:58 -0300

    feat: finished styles challengeBox

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "../styles/components/ChallengeBox.module.css";
+
+const ChallengeBox: React.FC = () => {
+  const hasActiveChallenge = true;
+  return (
+    <div className={styles.challengeBoxContainer}>
+      {hasActiveChallenge ? (
+        <div className={styles.challengeBoxActive}>
+          <header>Ganhe 400 xp</header>
+          <main>
+            <img src="icons/body.svg" />
+            <strong>Novo desafio</strong>
+            <p>Levante e faça uma caminhada de 3 minutos.</p>
+          </main>
+          <footer>
+            <button type="button" className={styles.challengeFaileButton}>
+              Falhei
+            </button>
+            <button type="button" className={styles.challengeSucceededButton}>
+              Completei
+            </button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeBoxNotActive}>
+          <strong>Finalize um ciclo para receber um desafio</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level up" />
+            Avance de level completando desafios
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ChallengeBox;
