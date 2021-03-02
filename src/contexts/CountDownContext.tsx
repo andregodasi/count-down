212012a34f33b1303bcb1527627c1ceda46058ec
andregodasi <andre.godasi@gmail.com>
2021-03-02 01:35:30 -0300

    fix: NaN and time 25 minutes

--- src/contexts/CountDownContext.tsx
+++ src/contexts/CountDownContext.tsx
@@ -27,7 +27,7 @@ export const CountDownContext = createContext({} as CountDownContextData);
 export function CountDownProvider({ children }: CountDownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.05 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -54,7 +54,7 @@ export function CountDownProvider({ children }: CountDownProviderProps) {
     clearTimeout(countdownTimeout);
     setIsActive(false);
     setHasFinished(false);
-    setTime(0.05 * 60);
+    setTime(25 * 60);
   }
 
   return (
